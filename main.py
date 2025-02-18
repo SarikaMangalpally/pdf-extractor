cb1ff0718e4a6730df4f0a8f135100eea9791cfe
Sarika Mangalpally <39065053+SarikaMangalpally@users.noreply.github.com>
2025-02-17 22:32:27 -0600

    changed orgins urls

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=[origins],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
